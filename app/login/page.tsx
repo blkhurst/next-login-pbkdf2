2cabbf3b77b1e5b250b8cf6d1075d30d30db23ce
Blkhurst <github@blkhurst.co.uk>
2025-04-09 14:59:17 +0100

    feat: add login & signup page

---
+++ app/login/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+import Link from "next/link";
+import { Label } from "@/components/ui/Label";
+import { Input } from "@/components/ui/Input";
+import { Button } from "@/components/ui/Button";
+import { useRef, useState } from "react";
+import { AuthFormState } from "@/lib/auth/definitions";
+
+export default function LoginPage() {
+  const handleSubmit = async () => {};
+  const [pending, setPending] = useState(false);
+  const [state, setState] = useState<AuthFormState>({});
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <main className="max-w-container mt-[20svh] max-w-sm text-center">
+      <div>
+        <h1 className="text-3xl font-medium tracking-tight">Login</h1>
+        <p className="text-copy-secondary text-sm">
+          Enter your credentials to access your account.
+        </p>
+      </div>
+
+      <div className="mt-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 text-left">
+          <div>
+            <Label>Email</Label>
+            <Input name="email" autoComplete="off" />
+          </div>
+
+          <div>
+            <Label>Password</Label>
+            <Input name="password" type="password" ref={passwordRef} />
+          </div>
+
+          <Button disabled={pending} type="submit" className="mt-2 w-full">
+            {pending ? "Processing..." : "Login"}
+          </Button>
+
+          {state?.error && (
+            <p className="text-sm text-red-500">{state.error}</p>
+          )}
+        </form>
+      </div>
+
+      <p className="mt-4 text-sm">
+        Don&apos;t have an account?{" "}
+        <Link className="underline" href="/signup">
+          Sign up
+        </Link>
+      </p>
+    </main>
+  );
+}
