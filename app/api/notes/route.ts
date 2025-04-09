ddb5a06664fd68cafa90e484fd468d0931ec81ec
Blkhurst <github@blkhurst.co.uk>
2025-04-09 19:05:06 +0100

    fix(api): resolve build errors
    I had also committed the incorrect file for users/route.ts. Ammended.

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,15 +1,15 @@
 import { auth } from "@/lib/auth/auth";
 import { db } from "@/lib/db/drizzle";
-import { notes } from "@/lib/db/schema";
+import { users } from "@/lib/db/schema";
 import { NextResponse } from "next/server";
 
-export const GET = auth(async function GET(req) {
-  const session = req.auth;
+export async function GET() {
+  const session = await auth();
 
   if (!session || session.user?.role !== "admin") {
     return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
   }
 
-  const data = await db.select().from(notes);
+  const data = await db.select().from(users);
   return NextResponse.json(data);
-});
+}
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,49 +1,15 @@
-// app/api/users/route.ts
-// import { db } from "@/lib/db/drizzle";
-// import { users } from "@/lib/db/schema";
-// import { NextResponse } from "next/server";
-import { eq, lt, gte, ne } from "drizzle-orm";
-
-// import { auth } from "@/lib/auth/auth";
-
-// export const GET = auth(async function GET(req) {
-//   const session = req.auth;
-//   if (!session || session.user?.role !== "admin")
-//     return NextResponse.json({ message: session }, { status: 401 });
-
-//   try {
-//     const data = await db.select().from(users);
-//     return NextResponse.json(data);
-//   } catch (err) {
-//     console.error("DB error:", err);
-//     return NextResponse.json("error", { status: 200 }); // Fallback to empty
-//   }
-// });
-
-// export async function GET() {
-//   try {
-//     // const data = await db.select({email: users.email}).from(users).where(eq(users.id, 1));
-//     const data = await db.select().from(users);
-//     return NextResponse.json(data);
-//   } catch (err) {
-//     console.error("DB error:", err);
-//     return NextResponse.json("error", { status: 200 }); // Fallback to empty
-//   }
-// }
-
-// app/api/users/route.ts
 import { auth } from "@/lib/auth/auth";
 import { db } from "@/lib/db/drizzle";
-import { users } from "@/lib/db/schema";
+import { notes } from "@/lib/db/schema";
 import { NextResponse } from "next/server";
 
-export const GET = auth(async function GET(req) {
-  const session = req.auth;
+export async function GET() {
+  const session = await auth();
 
   if (!session || session.user?.role !== "admin") {
     return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
   }
 
-  const data = await db.select().from(users);
+  const data = await db.select().from(notes);
   return NextResponse.json(data);
-});
+}
